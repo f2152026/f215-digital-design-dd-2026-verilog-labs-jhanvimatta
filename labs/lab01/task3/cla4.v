3333aca7550225e7a16e9d7b949be1fc57ecfa17
jhanvimatta <86959160+jhanvimatta@users.noreply.github.com>
2026-08-29 10:49:08 +0000

    lab01 task3

--- labs/lab01/task3/cla4.v
+++ labs/lab01/task3/cla4.v
@@ -31,7 +31,49 @@ module cla4(
   wire g0, g1, g2, g3;
   wire c1, c2, c3;
 
-  // TODO: your gate-level P/G, carry, and sum logic goes here.
-  // (cout should be connected to c4.) Remember the delay on every gate.
+  wire c1_t1;
+  wire c2_t1, c2_t2;
+  wire c3_t1, c3_t2, c3_t3;
+  wire c4_t1, c4_t2, c4_t3, c4_t4;
+
+  xor #(2) xor_p0 (p0, a[0], b[0]);
+  and #(2) and_g0 (g0, a[0], b[0]);
+
+  xor #(2) xor_p1 (p1, a[1], b[1]);
+  and #(2) and_g1 (g1, a[1], b[1]);
+
+  xor #(2) xor_p2 (p2, a[2], b[2]);
+  and #(2) and_g2 (g2, a[2], b[2]);
+
+  xor #(2) xor_p3 (p3, a[3], b[3]);
+  and #(2) and_g3 (g3, a[3], b[3]);
+
+  // c1 = g0 + (p0 & cin)
+  and #(2) a_c1_t1 (c1_t1, p0, cin);
+  or  #(2) o_c1    (c1, g0, c1_t1);
+
+  // c2 = g1 + (p1 & g0) + (p1 & p0 & cin)
+  and #(2) a_c2_t1 (c2_t1, p1, g0);
+  and #(2) a_c2_t2 (c2_t2, p1, p0, cin);
+  or  #(2) o_c2    (c2, g1, c2_t1, c2_t2);
+
+  // c3 = g2 + (p2 & g1) + (p2 & p1 & g0) + (p2 & p1 & p0 & cin)
+  and #(2) a_c3_t1 (c3_t1, p2, g1);
+  and #(2) a_c3_t2 (c3_t2, p2, p1, g0);
+  and #(2) a_c3_t3 (c3_t3, p2, p1, p0, cin);
+  or  #(2) o_c3    (c3, g2, c3_t1, c3_t2, c3_t3);
+
+  // c4 (cout) = g3 + (p3 & g2) + (p3 & p2 & g1) + (p3 & p2 & p1 & g0) + (p3 & p2 & p1 & p0 & cin)
+  and #(2) a_c4_t1 (c4_t1, p3, g2);
+  and #(2) a_c4_t2 (c4_t2, p3, p2, g1);
+  and #(2) a_c4_t3 (c4_t3, p3, p2, p1, g0);
+  and #(2) a_c4_t4 (c4_t4, p3, p2, p1, p0, cin);
+  or  #(2) o_c4    (cout, g3, c4_t1, c4_t2, c4_t3, c4_t4);
+
+  // sum[i] = p[i] ^ c[i] (where c[0] is cin)
+  xor #(2) xor_s0 (sum[0], p0, cin);
+  xor #(2) xor_s1 (sum[1], p1, c1);
+  xor #(2) xor_s2 (sum[2], p2, c2);
+  xor #(2) xor_s3 (sum[3], p3, c3);
 
 endmodule
